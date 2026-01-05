888f8f713263c993f6049f2ab692f1ecaed5dba1
pty10510 <pty10510@gmail.com>
2026-01-05 16:23:16 +0900

    0012

--- src/main/java/com/back/domain/post/post/respository/PostRepository.java
+++ src/main/java/com/back/domain/post/post/respository/PostRepository.java
@@ -1,8 +1,11 @@
 package com.back.domain.post.post.respository;
 
+import java.util.List;
+
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 import com.back.domain.post.post.document.Post;
 
 public interface PostRepository extends ElasticsearchRepository<Post, String> {
+	List<Post> findAll();
 }
